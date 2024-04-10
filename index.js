bc5e1879dd175f49dd4f23f0e9a3bed3a93f0836
Thorsten Sievert <sievert.tho@googlemail.com>
2024-04-10 16:11:03 +0000

    Solved Challange

--- index.js
+++ index.js
@@ -5,9 +5,17 @@
 var express = require('express');
 var app = express();
 
+app.use(function(reg, res, next) {
+  console.log(reg.method+" " + reg.path +" - " + reg.ip)
+  next()
+})
+
+
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
+const { param } = require('express/lib/application');
+const { json } = require('express/lib/response');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
@@ -30,3 +38,30 @@ app.get("/api/hello", function (req, res) {
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+app.get("/api/:date?", function(reg, res){
+  
+  let param = reg.params.date
+  
+  // check for empty parameter
+  if (typeof param == "undefined") {
+    param = new Date()
+  }
+  // check for unix timecode
+  else if (/^\d{5,}$/.test(param)) {
+    param = new Date(parseInt(param));
+  }
+
+  if (new Date(param) == "Invalid Date"){
+    res.json({
+      error: "Invalid Date" 
+    })
+  } else {
+    date = new Date (param)
+    res.json({
+      unix: date.valueOf(),
+      utc: date.toUTCString()
+    })
+}
+  
+})
